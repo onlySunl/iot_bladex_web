17c82aa454f3d2b4a16bd42b2d0e6b5095cab5c3
user6507384787 <1751938984068412-user6507384787@noreply.coze.cn>
2026-07-05 02:31:40 +0800

    refactor: 操作栏改为下拉菜单模式，所有操作放入更多菜单
    Coze-Commit-Type: user
    Coze-User-ID: 1751938984068412
    Coze-Conversation-ID: 9329079

--- src/views/nvr/qs/device/index.vue
+++ src/views/nvr/qs/device/index.vue
@@ -140,9 +140,9 @@
         </el-tag>
       </template>
 
-      <!-- ==================== 操作列扩展 ==================== -->
-      <!-- 使用 menu-left 插槽在默认按钮前添加自定义按钮 -->
-      <template #menu-left="{ row, size }">
+      <!-- ==================== 操作列 ==================== -->
+      <!-- 使用 menu 插槽完全控制操作列 -->
+      <template #menu="{ row, size }">
         <!-- 更多操作下拉菜单 -->
         <el-dropdown @command="(cmd) => handleCommand(cmd, row)" trigger="click">
           <el-button type="primary" text :size="size">
@@ -153,11 +153,16 @@
               <el-dropdown-item command="play">播放</el-dropdown-item>
               <el-dropdown-item command="snapshot">抓图</el-dropdown-item>
               <el-dropdown-item command="config">配置</el-dropdown-item>
+              <el-dropdown-item command="edit">编辑</el-dropdown-item>
+              <el-dropdown-item command="view">查看</el-dropdown-item>
               <el-dropdown-item command="reboot" :disabled="!canReboot(row)">重启</el-dropdown-item>
               <el-dropdown-item command="preset" :disabled="!isGb28181(row)">预置点</el-dropdown-item>
               <el-dropdown-item command="ptz" :disabled="!isGb28181(row)">云台控制</el-dropdown-item>
               <el-dropdown-item command="timeSync" :disabled="!canTimeSync(row)">校时</el-dropdown-item>
               <el-dropdown-item command="recordDownload" :disabled="!canDownloadRecord(row)">录像下载</el-dropdown-item>
+              <el-dropdown-item command="delete" divided>
+                <span style="color: var(--el-color-danger)">删除</span>
+              </el-dropdown-item>
             </el-dropdown-menu>
           </template>
         </el-dropdown>
@@ -907,6 +912,15 @@ export default {
           break
         case 'config':
           this.handleConfig(row)
+          break
+        case 'edit':
+          this.handleUpdate(row)
+          break
+        case 'view':
+          this.handleView(row)
+          break
+        case 'delete':
+          await this.handleDelete(row)
           break
         case 'reboot':
           await this.handleReboot(row)
